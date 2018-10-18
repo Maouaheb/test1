3b085c2e40eb7eff8255987e6541e4c50bb59630
Maweheb Belarbi <mawbelmawbel@gmail.com>
2018-10-18 14:28:44 +0200

    commit before push

--- Helloworld.java
+++ Helloworld.java
@@ -7,8 +7,7 @@ public class Helloworld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello after pull");
-                System.out.println("hello World Github");		
+		System.out.println("Hello to push");
 	}
 
 }
